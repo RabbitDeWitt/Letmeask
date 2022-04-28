33c810f5f6322622b9bf93c36b9c67eaec950e6f
David <davidsocoelho@outlook.com>
2022-04-28 00:04:11 -0300

    sexto commit

---
+++ letmeask/src/components/RoomCode.tsx
@@ -0,0 +1,24 @@
+import copyImg from '../assets/images/copy.svg'
+import '../styles/room-code.scss'
+
+type RoomCodeProps = {
+    code: string | undefined;
+}
+
+
+export function RoomCode(props: RoomCodeProps){
+    function copyRoomCodetoClipboard(){
+        if(props.code){
+        navigator.clipboard.writeText(props.code)
+    }
+    }
+
+    return(
+        <button className="room-code" onClick={copyRoomCodetoClipboard}>
+            <div>
+                <img src={copyImg} alt="Copy room code" />
+            </div>
+            <span>Sala #{props.code}</span>
+        </button>
+    )
+}
--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import { useNavigate } from 'react-router-dom'
+import { FormEvent, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
+
+import { database } from '../services/firebase';
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
@@ -11,14 +14,27 @@ import { Button } from '../components/Button'
 
 export function Home(){
     const navigate = useNavigate();
-    const { user ,signInWithGoogle } = useAuth()
+    const { user ,signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom(){
         if(!user){
            await signInWithGoogle()
         }
-
         navigate('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent){
+        event.preventDefault();
+        if (roomCode.trim()===''){
+            return;
+        }
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+        if (!roomRef.exists()){
+            alert('Room does not exists.');
+            return;
+        }
+        navigate(`/rooms/${roomCode}`)
     }
 
     return(
@@ -36,10 +52,12 @@ export function Home(){
                         Crie sua sala com o Google
                     </button>
                     <div className='separator'>ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text" 
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
